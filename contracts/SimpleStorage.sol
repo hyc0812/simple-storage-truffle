9a299e89b66d68196ed38ccde3013dd43bfc7eb6
yongchang he <hyc0812@163.com>
2022-04-01 15:36:32 -0600

    SimpleStorage created

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract SimpleStorage{
+    uint data;
+
+    function updateData(uint _data) external {
+        data = _data;
+    }
+
+    function readData() external view returns (uint) {
+        return data;
+    }
+
+}
